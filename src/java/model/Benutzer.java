63788c120a66c937f05ed49e675eea708d182dc2
LanglechnerMartin <langlechner.martin@wdgpocking.de>
2019-05-24 08:43:01 +0200

    Model classes

--- src/java/model/Benutzer.java
+++ src/java/model/Benutzer.java
@@ -10,8 +10,8 @@ public class Benutzer extends User {
 
     public Benutzer(String fn, String ln, String pw, String em, String st, char ge, int pc, int strn, Date bd, Status stat, String an, int bc, int pinN) {
         super(fn, ln, pw, em, st, ge, pc, strn, bd, stat);
-        accountNumber = accountNumber;
-        balance = balance;
+        accountNumber = an;
+        balance = bc;
         pin = pinN;
     }
 }
