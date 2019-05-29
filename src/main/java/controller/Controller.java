f339bf74997b0d9ee085d26393bfdd499633fce2
LanglechnerMartin <langlechner.martin@wdgpocking.de>
2019-05-29 07:49:55 +0200

    MainMenuView

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,8 @@
+package controller;
+
+public class Controller {
+    //Controller
+    public boolean loginButton(String EM, String pw){
+        return true;
+    }
+}
---
+++ src/main/java/model/Benutzer.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.Date;
+
+public class Benutzer extends User {
+
+    private String accountNumber;
+    private int balance;
+    private int pin;
+
+    public Benutzer(String fn, String ln, String pw, String em, String st, char ge, int pc, int strn, Date bd, Status stat, String an, int bc, int pinN) {
+        super(fn, ln, pw, em, st, ge, pc, strn, bd, stat);
+        accountNumber = an;
+        balance = bc;
+        pin = pinN;
+    }
+}
