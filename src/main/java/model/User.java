2c68fdd3252f1c209369acdb85e2a56b60f29c79
Daniel <IMashInYuGiOh@gmail.com>
2019-05-29 08:36:36 +0200

    User Account

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,106 +1,17 @@
 package model;
 
-import java.util.Date;
+        import java.util.Date;
 
-public abstract class User {
+public class User extends Account {
 
-    private String firstName, lastName, password, email, street;
-    private char gender;
-    private int postalCode, streetNumber;
-    private Date birthdate;
-    private Status status;
+    private String accountNumber;
+    private int balance;
+    private int pin;
 
-    public User(String fn, String ln, String pw, String em, String st, char ge,
-                int pc, int strn, Date bd, Status stat){
-        firstName = fn;
-        lastName = ln;
-        password = pw;
-        email = em;
-        street = st;
-        gender = ge;
-        postalCode = pc;
-        streetNumber = strn;
-        birthdate = bd;
-        status = stat;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public char getGender() {
-        return gender;
-    }
-
-    public void setGender(char gender) {
-        this.gender = gender;
-    }
-
-    public int getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(int postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public int getStreetNumber() {
-        return streetNumber;
-    }
-
-    public void setStreetNumber(int streetNumber) {
-        this.streetNumber = streetNumber;
-    }
-
-    public Date getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(Date birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
+    public User(String fn, String ln, String pw, String em, String st, char ge, int pc, int strn, Date bd, Status stat, String an, int bc, int pinN) {
+        super(fn, ln, pw, em, st, ge, pc, strn, bd, stat);
+        accountNumber = an;
+        balance = bc;
+        pin = pinN;
     }
 }
