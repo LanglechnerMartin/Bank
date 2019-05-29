8e0c296310bec60de9f171775fcd9419c98ac891
LanglechnerMartin <langlechner.martin@wdgpocking.de>
2019-05-29 08:25:57 +0200

    NavigationView

---
+++ src/main/java/controller/ControllerNavigation.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerNavigation {
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ResourceBundle resources;
+
+    public ControllerNavigation() { }
+
+    @FXML
+    public void initialize() { }
+
+}
--- src/main/java/view/Navigation.java
+++ src/main/java/view/Navigation.java
@@ -6,11 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainMenu extends Application {
+public class Navigation extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MainMenu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Navigation.fxml"));
         primaryStage.setTitle("WDG-Bank");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
