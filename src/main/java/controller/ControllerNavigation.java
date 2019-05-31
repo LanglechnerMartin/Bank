f78d096605bc2d95977421fb5723cc07d2dde311
LanglechnerMartin <langlechner.martin@wdgpocking.de>
2019-05-31 09:16:01 +0200

    AlleViews

--- src/main/java/controller/ControllerNavigation.java
+++ src/main/java/controller/ControllerNavigation.java
@@ -1,11 +1,18 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
 public class ControllerNavigation {
+
+    @FXML
+    private AnchorPane rootPane;
 
     @FXML
     private URL location;
@@ -20,7 +27,14 @@ public class ControllerNavigation {
 
     @FXML
     public void logout(){
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getClassLoader().getResource("MainMenu.fxml"));
+            rootPane.getChildren().setAll(pane);
 
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
